e056d6fdc05db9d27a30a30669b86e71bf822f53
96sMicks <melkiasae@hotmail.com>
2021-10-16 22:08:43 +0000

    Made the 3 methods, added the http authentication to the admin/catgories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,13 @@
 class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with :name => 'Jungle', :password => 'book'
+  
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
 end
