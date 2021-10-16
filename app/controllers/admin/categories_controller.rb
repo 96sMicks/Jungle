5cf6162830f5c2db19b90045a253f379e3225ad3
96sMicks <melkiasae@hotmail.com>
2021-10-16 22:40:35 +0000

    Made a catgories instance variable, that is passed into admin/category/index and made a catergory table row template based on product's partial and table

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,8 +1,9 @@
 class Admin::CategoriesController < ApplicationController
 
   http_basic_authenticate_with :name => 'Jungle', :password => 'book'
-  
+
   def index
+    @categories = Category.all
   end
 
   def show
