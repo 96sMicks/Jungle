c067224cc7aa781508f118dbe259ec31e85d1d8c
96sMicks <melkiasae@hotmail.com>
2021-10-16 23:27:18 +0000

    The index page for admin/categories made with details about how many products are in each category using SQL

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,7 +3,8 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with :name => 'Jungle', :password => 'book'
 
   def index
-    @categories = Category.all
+    @categories = Category.joins("LEFT JOIN products ON products.category_id = categories.id")
+    .select('categories.name, count(products.*)').group('categories.id')
   end
 
   def show
