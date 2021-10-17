f83be86205428bf99746e0309b6bb43dc0a2949a
96sMicks <melkiasae@hotmail.com>
2021-10-17 04:10:47 +0000

    The new and create methods now work as intended. Also linked the new category button to the new page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -7,9 +7,23 @@ class Admin::CategoriesController < ApplicationController
     .select('categories.name, count(products.*)').group('categories.id')
   end
 
-  def show
+  def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
