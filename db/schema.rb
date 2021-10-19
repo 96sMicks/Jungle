4afae9ecb1a7103dd32401cb62bb7118b2e8588c
96sMicks <melkiasae@hotmail.com>
2021-10-19 23:32:25 +0000

    updated the schema to include user name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211017170931) do
+ActiveRecord::Schema.define(version: 20211018200059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
