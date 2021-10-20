465803b028c2552cb37fc955932a3a36c4666527
96sMicks <melkiasae@hotmail.com>
2021-10-20 18:19:39 +0000

    The files for a tutorial into testing with poltigeist and capybara to test feattures

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor can navigate from the homepage to the product detail page by clicking on a product", type: :feature do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+
+  
+end
